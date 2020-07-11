f58921e4d5b5fd266bb6787024a6675b960ff6e2
StardustDL <stardustdl@163.com>
2020-07-11 19:14:24 +0800

    fix origin postasjson bug, use stringcontent instead

---
+++ src/Judge0/HttpClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Judge0
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsJsonAsync<TValue>(this HttpClient client, string? requestUri, TValue value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+            StringContent content = new StringContent(JsonSerializer.Serialize(value), Encoding.UTF8, "application/json");
+            return client.PostAsync(requestUri, content, cancellationToken);
+        }
+    }
+}
